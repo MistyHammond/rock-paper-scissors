6279fdad346ef94def9bbf8325bc2ce9b4ae9b59
Misty <hammondmha@gmail.com>
2022-11-01 16:11:35 +0000

    Added a scoring mechanic along with a way to start a new round

--- rps.js
+++ rps.js
@@ -1,4 +1,7 @@
 var choices = ["Rock", "Paper", "Scissors"];
+let playerLives = 5;
+let computerLives = 5;
+let round = 0;
 
 function getComputerChoice() {
     const computerChoice = choices[Math.floor(Math.random() * choices.length)];
@@ -12,24 +15,33 @@ function compare(computerChoice, playerChoice) {
         // Player Win Statements
     } else if ((computerChoice === "Scissors" && playerChoice === "Rock") || (computerChoice === "Paper" && playerChoice === "Scissors") || (computerChoice === "Rock" && playerChoice === "Paper")) {
         document.getElementById("result").innerHTML = "You Win!";
+        computerLives -= 1;
         // Player Lose Statements
     } else if ((computerChoice === "Scissors" && playerChoice === "Paper") || (computerChoice === "Paper" && playerChoice === "Rock") || (computerChoice === "Rock" && playerChoice === "Scissors")) {
         document.getElementById("result").innerHTML = "You Lose!";
+        playerLives -= 1;
     }
+    document.getElementById("player-lives").innerHTML = `Player lives: ${playerLives}`;
+    document.getElementById("computer-lives").innerHTML = `Computer lives: ${computerLives}`;
 }
 
 function playRound(playerInput) {
+    if (playerLives === 0 || computerLives === 0) {
+        return
+        } 
     console.log(playerInput);
     var computerInput = getComputerChoice();
     document.getElementById("computer-output").innerHTML = `Computer chose ${computerInput}`;
     compare(computerInput, playerInput);
+    if (playerLives === 0 || computerLives ===0) { 
+    document.getElementById("round-container").style.display ='flex';
+    }
 }
 
-/*
-function playRound() {
-    var playerInput = document.getElementById("playerInput").value;
-    console.log(playerInput);
-    var computerInput = getComputerChoice();
-    compare(computerInput, playerInput);
+function newRound() {
+    playerLives = 5;
+    computerLives = 5;
+    document.getElementById("player-lives").innerHTML = `Player lives: ${playerLives}`;
+    document.getElementById("computer-lives").innerHTML = `Computer lives: ${computerLives}`;
+    document.getElementById("round-container").style.display ='none';
 }
-*/
